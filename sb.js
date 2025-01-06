dce0a97e7efab24f26b353a6e56d1f6422b97016
Subham Sangwan <subhamsangwan26@gmail.com>
2025-01-06 17:54:06 +0530

    RePosted

---
+++ sb.js
@@ -0,0 +1,32 @@
+setInterval(setClock, 1000);
+
+const hourHand = document.querySelector('[data-hour-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const secondHand = document.querySelector('[data-second-hand]');
+const digitalClock = document.getElementById('digital-clock');
+
+function setClock() {
+  const currentDate = new Date();
+  const secondsRatio = currentDate.getSeconds() / 60;
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+
+  setRotation(secondHand, secondsRatio);
+  setRotation(minuteHand, minutesRatio);
+  setRotation(hourHand, hoursRatio);
+
+  setDigitalClock(currentDate);
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360);
+}
+
+function setDigitalClock(date) {
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  digitalClock.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+setClock();
